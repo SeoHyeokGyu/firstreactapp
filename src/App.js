92c8d6ac591259278a853e2ee1e25a72296ed016
SeoHyeokGyu <shk64099@gmail.com>
2021-05-03 22:25:37 +0900

    #useState 1. 랜더링 시점은 상태값 변경 2. userState 는 hooks 라이브러리 상태값 3. setNumber는 리액트에게 number값 변경 요청 4. 다시 그릴지 안그릴지 함수 제공

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import Sub from "./sub";
 
 function App() {
+    //let number = 1;
+    const [number,setNumber] = useState(1);
+
+    const add = () => {
+        setNumber(number+1);
+        console.log('add',number);
+    };
+
   return (
       <div>
-          안녕
-          <hr />
+          <h1>숫자 :{number}</h1>
+          <button onClick={add}>더하기</button>
+          <Sub></Sub>
       </div>
-
   );
 }
 
