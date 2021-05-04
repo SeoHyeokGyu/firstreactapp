529d0216bf975646cb47826e3b1484ab75f5d0e2
SeoHyeokGyu <shk64099@gmail.com>
2021-05-04 17:55:53 +0900

    #useMemo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import './App.css';
-import {useEffect, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import Sub from "./sub";
 import {cleanup} from "@testing-library/react";
 
 function App() {
 
-    const [data,setData] = useState(0);
-    useEffect(()=>{
+    const [list,setList] = useState([1,2,3,4]);
+    const [str,setStr] = useState("합계")
 
-        const download = () =>{
-            let downloadData = 5;
-            setData(downloadData);
 
-        }
-       console.log("useEffect");
-       download();
-    }, []);
 
+    const getAddResult = () => {
+        let sum = 0;
+        list.forEach((i)=> (sum=sum+i));
+        return sum;
+    }
+    const addResult = useMemo(()=>getAddResult(), [list]);
   return (
       <div>
-          <h1>데이터:{data}</h1>
-          <button onClick={()=>{setData(data+1)}}>더하기</button>
+          <button
+              onClick={()=>{
+                  setStr("안녕")
+              }}
+          >문자변경
+          </button>
+          <button
+              onClick={()=>{
+                  setList([...list,5]);
+          }}
+          >더하기
+          </button>
+          <div>
+              {list.map((i)=>(
+                  <h1>{i}</h1>
+          ))}
+          </div>
+          <div>{str}:{addResult}</div>
       </div>
   );
 }
