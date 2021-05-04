af150f024148b46473f156000cf9108e8629ac53
SeoHyeokGyu <shk64099@gmail.com>
2021-05-04 19:16:43 +0900

    #useRef 1. createRef로 State생성하고 2. State를 ref에 지정하면 3. State의 .current를 통하여 사용 가능하다.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,29 @@
 import './App.css';
-import {useEffect, useMemo, useState} from "react";
+import {createRef, useEffect, useMemo, useRef, useState} from "react";
 import Sub from "./sub";
 import {cleanup} from "@testing-library/react";
 
 function App() {
 
-    const [list,setList] = useState([1,2,3,4]);
-    const [str,setStr] = useState("합계")
+  const myRef = useRef(null);
 
+  const [list,setList] = useState([
+      {id:1,name:"홍길동"},
+      {id:2,name:"김길동"}
+  ]);
+    const myRefs = Array.from({length:list.length}).map(()=>createRef());
 
-
-    const getAddResult = () => {
-        let sum = 0;
-        list.forEach((i)=> (sum=sum+i));
-        return sum;
-    }
-    const addResult = useMemo(()=>getAddResult(), [list]);
   return (
       <div>
-          <button
-              onClick={()=>{
-                  setStr("안녕")
-              }}
-          >문자변경
+          <button onClick={()=>{
+              myRefs[0].current.style.backgroundColor='red';
+          }}>
+              변경
           </button>
-          <button
-              onClick={()=>{
-                  setList([...list,5]);
-          }}
-          >더하기
-          </button>
-          <div>
-              {list.map((i)=>(
-                  <h1>{i}</h1>
-          ))}
-          </div>
-          <div>{str}:{addResult}</div>
+          <div ref={myRef}>박스</div>
+          {list.map((user,index) => (
+              <h1 ref={myRefs[index]}>{user.name}</h1>
+              ))}
       </div>
   );
 }
